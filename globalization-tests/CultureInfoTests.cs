4d27b81575cb3d601762ee69f8b0bbd7eb3743f1
alinciuca <alinciuca@live.com>
2018-09-02 12:22:49 +0300

    added tests for the internationalization

---
+++ globalization-tests/CultureInfoTests.cs
@@ -0,0 +1,60 @@
+﻿using NUnit.Framework;
+using System.Globalization;
+using System.Reflection;
+using System.Resources;
+using System.Threading;
+using globalization_resources;
+
+namespace globalization_tests
+{
+    [TestFixture]
+    public class CultureInfoTests
+    {
+        [Test]
+        public void ReturnCurrentCulture()
+        {
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+
+            Assert.AreEqual("en", currentCulture.TwoLetterISOLanguageName);
+        }
+
+        [Test]
+        public void ReturnCurrentUiCulture()
+        {
+            var currentUiCulture = Thread.CurrentThread.CurrentUICulture;
+            Assert.AreEqual("en", currentUiCulture.TwoLetterISOLanguageName);
+        }
+
+        [Test]
+        public void SetNeutralCulture()
+        {
+            var cultureInfo = new CultureInfo("en");
+            Thread.CurrentThread.CurrentCulture = cultureInfo;
+            var currentCulture = Thread.CurrentThread.CurrentCulture;
+            Assert.IsTrue(currentCulture.IsNeutralCulture);
+        }
+
+        [Test]
+        public void GetDefaultResource()
+        {
+            //reset culture
+            Resources.Culture = null;
+            Assert.AreEqual("this is a test", Resources.Test);
+        }
+
+        [Test]
+        public void GetCultureSpecificResource()
+        {
+            //set culture
+            Resources.Culture = new CultureInfo("ro");
+            Assert.AreEqual("Acesta este un test", Resources.Test);
+        }
+
+        [Test]
+        public void Fallback()
+        {
+            Resources.Culture = new CultureInfo("ro");
+            Assert.IsNotNull(Resources.FallbackTest);
+        }
+    }
+}
---
+++ globalization-tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("globalization-tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("globalization-tests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("782bd363-039b-4ac4-a81f-1007e204f91c")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
